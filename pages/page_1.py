772e4acc512ee436ea16338080dd4d186c0b9d4a
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-05-31 05:26:36 +0800

    Update page_1.py

--- pages/page_1.py
+++ pages/page_1.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import base64
+import google.auth
+from google.oauth2 import service_account
+import google.auth.transport.requests
 import vertexai
 from vertexai.generative_models import GenerativeModel, Part, FinishReason
 import vertexai.preview.generative_models as generative_models

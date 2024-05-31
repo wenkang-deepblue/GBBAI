9f90e74035f8801c0322cf357962864e1a59a651
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-05-31 15:47:34 +0800

    Update page_1.py

--- pages/page_1.py
+++ pages/page_1.py
@@ -11,8 +11,10 @@ import json
 
 credentials_info = st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]
 
+credentials_dict = json.loads(credentials_info)
+
 creds = service_account.Credentials.from_service_account_info(
-    credentials_info,
+    credentials_dict,
     scopes=["https://www.googleapis.com/auth/cloud-platform"]
 )
 

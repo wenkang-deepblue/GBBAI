a5bab32a1d6fb7cfcd1d4bdb1236869299fd77db
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-05-31 22:06:04 +0800

    Update page_1.py

--- pages/page_1.py
+++ pages/page_1.py
@@ -6,8 +6,6 @@ import google.auth.transport.requests
 import vertexai
 from vertexai.generative_models import GenerativeModel, Part, FinishReason
 import vertexai.preview.generative_models as generative_models
-import os
-import json
 
 credentials_info = st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]
 

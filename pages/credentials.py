e24ec77b3fbe0a1d42a579beb95b82595ee8cf3f
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-05-31 14:26:48 +0800

    Create credentials.py

---
+++ pages/credentials.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import base64
+import google.auth
+from google.oauth2 import service_account
+import google.auth.transport.requests
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part, FinishReason
+import vertexai.preview.generative_models as generative_models
+import os
+import json
+
+credentials_info = st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]
+
+st.write(credentials_info)

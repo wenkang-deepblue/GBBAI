f27f3a3c8ae21a1f9b846b7795c825091c68d4a1
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-08-06 17:04:04 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,4 +1,3 @@
-#import streamlit lib:
 import streamlit as st
 
 #this will showed on the top of user's
@@ -7,15 +6,15 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.image("https://storage.googleapis.com/ghackathon/galaxy%20banner%20with%20logo.png")
+st.image("../rag-demo/pdf/galaxy banner with logo.png")
 
 
-st.write("# Hello! Welcome to :blue[GBB] :rainbow[AI]!")
+st.write("# 您好！欢迎使用 :blue[GBB] :rainbow[AI] !")
 
 
 st.markdown(
     """
-    The GBB AI project is a GenAI system built on :blue[Google Cloud Vertex AI] platform, aiming to demonstrate the capabilities of various Vertex AI modules for enterprises, such as content generation, media comprehension, RAG search, and media search. The Vertex AI modules used in this project include :orange[Gemini 1.5 Pro multimodal model, Agent Builder - Vertex AI Search, Imagen, DialogFlow]. Google Cloud China sales and engineering team is willing to fully assist you in building enterprise-level AI applications by leveraging Google's powerful AI foundation capabilities, as well as GCP's comprehensive AI ecosystem and technical architecture, to help your enterprise iterate quickly, develop flexibly, reduce costs, and improve efficiency.
+    GBB AI项目是利用:blue[Google Cloud Vertex AI]平台搭建的GenAI系统，其目的是演示Vertex AI各个模块可为企业实现的内容生成，媒体理解，RAG检索增强生成以及媒体搜索等功能。该项目所用到的Vertex AI模块包括：:orange[Gemini 1.5 Pro多模态模型，Agent Builder - Vertex AI Search，Imagen，DialogFlow]等等。Google Cloud中国销售及架构师团队愿意全力协助您利用Google强大的AI基础能力，以及GCP全面的AI生态及技术架构，搭建企业级的AI应用，帮助您的企业快速迭代，灵活开发，降低成本，提高效率。
     
     
     
@@ -24,7 +23,7 @@ st.markdown(
 
 st.markdown(
     """
-    **👈 Please click on the left for experiencing!**
+    **👈 请点击左边开始体验吧！**
     
     
 """
@@ -33,27 +32,32 @@ st.markdown(
 with st.sidebar:
     left_co, cent_co,last_co = st.columns([0.34,0.33,0.33])
     with cent_co:
-        st.image('https://storage.googleapis.com/ghackathon/image2.gif')
+        st.image('../rag-demo/pdf/image2.gif')
     left_co, cent_co,last_co = st.columns([0.36,0.32,0.32])
     with cent_co:
         st.title(":blue[GBB] :rainbow[AI]")
-    st.page_link("homepage.py", label="Homepage", icon="🏠")
-    st.page_link("pages/page_1.py", label="Article Generation", icon="📖")
-    st.page_link("pages/page_2.py", label="Media Comprehension", icon="🎞️")
-    st.page_link("pages/page_3.py", label="Text Translation", icon="🇺🇳")
-    st.page_link("pages/page_4.py", label="Document Search", icon="🔍")
-    st.page_link("pages/page_5.py", label="Media Search", icon="🎥")
-    st.page_link("pages/page_6.py", label="Image Generation", icon="🎨")
-    st.page_link("pages/page_7.py", label="Customer Service Chatbot", icon="🤖")
-    st.page_link("https://pantheon.corp.google.com/vertex-ai/generative/multimodal/create/text?project=lwk-genai-test", label="GCP Console - Gemini", icon="🌎")
-    st.page_link("https://pantheon.corp.google.com/gen-app-builder/locations/global/engines/lwk-rag-search_1713579191717/preview/search?e=13803378&mods=dm_deploy_from_gcs&project=lwk-genai-test", label="GCP Console - Vertex AI Searh", icon="🌎")
+    st.page_link("homepage.py", label="主页", icon="🏠")
+    st.page_link("pages/page_0.py", label="文本生成", icon="📖")
+    st.page_link("pages/page_9.py", label="视频理解", icon="🎞️")
+    st.page_link("pages/page_13.py", label="文本翻译", icon="🇺🇳")
+    st.page_link("pages/page_2.py", label="RAG搜索", icon="🔍")
+    st.page_link("pages/page_3.py", label="媒体搜索", icon="🎥")
+    st.page_link("pages/page_16.py", label="图片生成", icon="🎨")
+    st.page_link("pages/page_18.py", label="聊天机器人", icon="💬")
+    st.page_link("pages/page_15.py", label="游戏客服平台", icon="🤖")
+    st.page_link("pages/page_21.py", label="电商客服平台", icon="🤖")
+    st.page_link("pages/page_19.py", label="Claude3.5聊天机器人", icon="💬")
+    st.page_link("pages/page_23.py", label="Llama3.1聊天机器人", icon="💬")
+    st.page_link("https://translationhub.cloud.google.com/portal/cbec99246ab9ab5?projectId=210890376426", label="GCP翻译门户", icon="🌎")
+    st.page_link("https://pantheon.corp.google.com/vertex-ai/generative/multimodal/create/text?project=lwk-genai-test", label="GCP控制台 - Gemini", icon="🌎")
+    st.page_link("https://pantheon.corp.google.com/gen-app-builder/locations/global/engines/lwk-rag-search_1713579191717/preview/search?e=13803378&mods=dm_deploy_from_gcs&project=lwk-genai-test", label="GCP控制台 - RAG搜索", icon="🌎")
     st.text("")
     st.subheader('', divider='rainbow')
     st.text("")
     st.markdown(
         """
-    ## About
-    This is an enterprise readiness GenAI platform powered by :blue[Google Cloud Vertex AI]
+    ## 关于
+    这是由:blue[Google Cloud Vertex AI]驱动的生成式AI平台以及企业级RAG搜索引擎
     - [:cloud: Google Cloud Vertex AI](https://cloud.google.com/vertex-ai?hl=en)
 
     """
@@ -63,16 +67,12 @@ with st.sidebar:
     st.text("")
     st.text("")
     st.text("")
-    left_co, cent_co,last_co = st.columns([0.3,0.4,0.30])
+    left_co, cent_co,last_co = st.columns([0.39,0.31,0.30])
     with cent_co:
-        st.write('© [Wenkang Li](https://moma.corp.google.com/person/wenkangli?q=image%20generatioin%20streamlit)')
-    left_co, cent_co,last_co = st.columns([0.2,0.79,0.1])
+        st.write('© GBB')
+    left_co, cent_co,last_co = st.columns([0.09,0.83,0.08])
     with cent_co:
-        st.write(
-        '''
-        :grey[Designed & Developed by]
-        :blue[Wenkang Li & Gunther Hua]'''
-         )
+        st.write(':grey[Designed & Developed by] :blue[李文康]')
     left_co, cent_co,last_co = st.columns([0.22,0.6,0.18])
     with cent_co:
         st.write(':grey[Powered by] **Vertex AI**')

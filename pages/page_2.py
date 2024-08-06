be9e89658f73839f0eca8d3093682c5609dd78e8
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-08-06 17:21:35 +0800

    Update page_2.py

--- pages/page_2.py
+++ pages/page_2.py
@@ -19,23 +19,21 @@ creds = service_account.Credentials.from_service_account_info(
 auth_req = google.auth.transport.requests.Request()
 creds.refresh(auth_req)
 
-vertexai.init(project="lwk-genai-test", location="us-central1", credentials=creds)
-storage_client = storage.Client(credentials=creds)
-
+vertexai.init(project="lwk-genai-test", location="us-central1")
 model = GenerativeModel("gemini-1.5-flash-001")
-# Streamlit UI
+# Streamlit 应用界面
 left_co, cent_co,last_co = st.columns([0.39,0.31,0.30])
 with cent_co:
     st.title(":blue[GBB] :rainbow[AI]")
-left_co, cent_co,last_co = st.columns([0.3,0.6,0.1])
+left_co, cent_co,last_co = st.columns([0.39,0.31,0.3])
 with cent_co:
-    st.caption(":blue[_Enterprise Text Content Generation Platform_]")
-st.image('https://storage.googleapis.com/ghackathon/page_2.png')
+    st.caption(":blue[_企业级内容生成平台_]")
+st.image('https://storage.googleapis.com/ghackathon/page_1_zh.png')
 left_co, cent_co,last_co = st.columns([0.24,0.51,0.25])
 with cent_co:
     st.subheader('', divider='rainbow')
 
-#continue streamlit sidebar UI
+#继续streamlit sidebar界面
 with st.sidebar:
     left_co, cent_co,last_co = st.columns([0.34,0.33,0.33])
     with cent_co:
@@ -43,31 +41,33 @@ with st.sidebar:
     left_co, cent_co,last_co = st.columns([0.36,0.32,0.32])
     with cent_co:
         st.title(":blue[GBB] :rainbow[AI]")
-    temperature = st.slider("Adjust Temperature", min_value=0.0, max_value=2.0, value=1.0, help=(
+    temperature = st.slider("调整模型Temperature", min_value=0.0, max_value=2.0, value=1.0, help=(
         """
-        Temperature controls the randomness in token selection \n
-
-        -A lower temperature is good when you expect a true or correct response. A temperature of 0 means the highest probability token is always selected. \n
-        -A higher temperature can lead to diverse or unexpected results. Some models have a higher temperature max to encourage more random responses. \n
-        The selected model gemini-1.5-flash-001 has a temperature range of 0 - 2 and a default of 1.0
+        Temperature用于响应生成期间的采样，这发生在应用 topP 和 topK 时。Temperature控制了token选择中的随机程度。对于需要较少开放式或创造性响应的提示，较低的temperature是好的，而较高的temperature可以导致更多样化或创造性的结果。Temperature为 0 意味着始终选择最高概率的token。在这种情况下，给定提示的响应大多是确定的，但仍有可能出现少量变化。
+        
+        如果模型返回的响应过于通用、太短或模型给出回退响应，请尝试提高temperature。
         """
     ))
-    top_p = st.slider ("Adjust Top_p", min_value=0.00, max_value=1.00, value=0.95, help=(
+    top_p = st.slider ("调整模型Top_p", min_value=0.00, max_value=1.00, value=0.95, help=(
         """
-        Top-p changes how the model selects tokens for output. Tokens are selected from most probable to least until the sum of their probabilities equals the top-p value. For example, if tokens A, B, and C have a probability of .3, .2, and .1 and the top-p value is .5, then the model will select either A or B as the next token (using temperature).
+        Top-P 改变了模型选择输出tokens的方式。Tokens按照从最可能（见top-K）到最不可能的顺序进行选择，直到它们的概率之和等于top-P值。例如，如果token A、B和C的概率分别为0.3、0.2和0.1，top-P值为0.5，那么模型将使用温度从A或B中选择下一个token，并排除C作为候选。
+
+        指定较低的值会得到较少的随机响应，指定较高的值会得到更多的随机响应。
         """
     ))
     st.subheader('',divider='rainbow')
 
-    uploaded_file = st.file_uploader("Upload file to Google Cloud Storage", type=("mp4", "wmv", "jpg", "png"))
+    uploaded_file = st.file_uploader("上传文件到 Google Cloud Storage", type=("mp4", "wmv", "jpg", "png"))
     
+    #定义全局可用
     file_type = None
     
     if uploaded_file is not None:
         file_type = uploaded_file.name.split(".")[-1]
    
-    #define file uploading function
+    #定义上传文件到存储桶的函数
     def upload_to_gcs(uploaded_file, bucket_name, destination_blob_name, source_file_name):
+        """将文件上传到 Google Cloud Storage"""
         with tempfile.NamedTemporaryFile(delete=False) as temp_file:
                 temp_file.write(uploaded_file.read())
                 temp_file_name = temp_file.name
@@ -82,44 +82,49 @@ with st.sidebar:
         
         blob.upload_from_filename(temp_file_name, if_generation_match=generation_match_precondition)               
             
-        # delete temp file
+        #删除临时文件
         os.remove(temp_file_name)
     
         return f"gs://{bucket_name}/{destination_blob_name}"
           
     with st.form("mysidebarform"):
-        left_co, cent_co,last_co = st.columns([0.32,0.38,0.3])
+        left_co, cent_co,last_co = st.columns([0.35,0.33,0.33])
         with cent_co:
-            submitted = st.form_submit_button("Upload")
+            submitted = st.form_submit_button("上传")
         
         if submitted and not uploaded_file:
-            st.markdown('<font size="2" color="#EA4335">Please choose your file firstly</font>', unsafe_allow_html=True)
+            st.markdown('<font size="2" color="#EA4335">请先选择文件</font>', unsafe_allow_html=True)
         
         if submitted and uploaded_file:
-            with st.spinner('Uploading, a moment please...'):
+            with st.spinner('正在上传，请稍等...'):
                 gs_uri = upload_to_gcs(uploaded_file, "your-bucket-name", uploaded_file.name, uploaded_file.name)
-                st.markdown('<font size="2" color="#1E8E3E">The file has been successfully uploaded to：</font>', unsafe_allow_html=True)
+                st.markdown('<font size="2" color="#1E8E3E">文件已经成功上传至：</font>', unsafe_allow_html=True)
                 st.code(gs_uri)
          
-    st.page_link("https://pantheon.corp.google.com/storage/browser/lwk-rag-videos;tab=objects?forceOnBucketsSortingFiltering=true&e=13803378&hl=en&mods=dm_deploy_from_gcs&project=lwk-genai-test&prefix=&forceOnObjectsSortingFiltering=false", label="Google Cloud Storage Bucket", icon="🌎")
+    st.page_link("https://pantheon.corp.google.com/storage/browser/lwk-rag-videos;tab=objects?forceOnBucketsSortingFiltering=true&e=13803378&hl=en&mods=dm_deploy_from_gcs&project=lwk-genai-test&prefix=&forceOnObjectsSortingFiltering=false", label="Google Cloud存储桶", icon="🌎")
     st.subheader('',divider='rainbow')
-    st.page_link("homepage.py", label="Homepage", icon="🏠")
-    st.page_link("pages/page_1.py", label="Article Generation", icon="📖")
-    st.page_link("pages/page_2.py", label="Media Comprehension", icon="🎞️")
-    st.page_link("pages/page_3.py", label="Text Translation", icon="🇺🇳")
-    st.page_link("pages/page_4.py", label="Document Search", icon="🔍")
-    st.page_link("pages/page_5.py", label="Media Search", icon="🎥")
-    st.page_link("pages/page_6.py", label="Image Generation", icon="🎨")
-    st.page_link("pages/page_7.py", label="Customer Service Chatbot", icon="🤖")
-    st.page_link("https://pantheon.corp.google.com/vertex-ai/generative/multimodal/create/text?project=lwk-genai-test", label="GCP Console - Gemini", icon="🌎")
-    st.page_link("https://pantheon.corp.google.com/gen-app-builder/locations/global/engines/lwk-rag-search_1713579191717/preview/search?e=13803378&mods=dm_deploy_from_gcs&project=lwk-genai-test", label="GCP Console - Vertex AI Searh", icon="🌎")
+    st.page_link("homepage.py", label="主页", icon="🏠")
+    st.page_link("pages/page_0.py", label="文本生成", icon="📖")
+    st.page_link("pages/page_9.py", label="视频理解", icon="🎞️")
+    st.page_link("pages/page_13.py", label="文本翻译", icon="🇺🇳")
+    st.page_link("pages/page_2.py", label="RAG搜索", icon="🔍")
+    st.page_link("pages/page_3.py", label="媒体搜索", icon="🎥")
+    st.page_link("pages/page_16.py", label="图片生成", icon="🎨")
+    st.page_link("pages/page_18.py", label="聊天机器人", icon="💬")
+    st.page_link("pages/page_15.py", label="游戏客服平台", icon="🤖")
+    st.page_link("pages/page_21.py", label="电商客服平台", icon="🤖")
+    st.page_link("pages/page_19.py", label="Claude3.5聊天机器人", icon="💬")
+    st.page_link("pages/page_23.py", label="Llama3.1聊天机器人", icon="💬")
+    st.page_link("https://translationhub.cloud.google.com/portal/cbec99246ab9ab5?projectId=210890376426", label="GCP翻译门户", icon="🌎")
+    st.page_link("https://pantheon.corp.google.com/vertex-ai/generative/multimodal/create/text?project=lwk-genai-test", label="GCP控制台 - Gemini", icon="🌎")
+    st.page_link("https://pantheon.corp.google.com/gen-app-builder/locations/global/engines/lwk-rag-search_1713579191717/preview/search?e=13803378&mods=dm_deploy_from_gcs&project=lwk-genai-test", label="GCP控制台 - RAG搜索", icon="🌎")
     st.text("")
     st.subheader('', divider='rainbow')
     st.text("")
     st.markdown(
         """
-    ## About
-    This is an enterprise readiness GenAI platform powered by :blue[Google Cloud Vertex AI]
+    ## 关于
+    这是由Google Cloud Vertex AI驱动的生成式AI平台以及企业级RAG搜索引擎
     - [:cloud: Google Cloud Vertex AI](https://cloud.google.com/vertex-ai?hl=en)
 
     """
@@ -129,21 +134,17 @@ with st.sidebar:
     st.text("")
     st.text("")
     st.text("")
-    left_co, cent_co,last_co = st.columns([0.3,0.4,0.30])
+    left_co, cent_co,last_co = st.columns([0.39,0.31,0.30])
     with cent_co:
-        st.write('© [Wenkang Li](https://moma.corp.google.com/person/wenkangli?q=image%20generatioin%20streamlit)')
-    left_co, cent_co,last_co = st.columns([0.2,0.79,0.1])
+        st.write('© GBB')
+    left_co, cent_co,last_co = st.columns([0.09,0.83,0.08])
     with cent_co:
-        st.write(
-        '''
-        :grey[Designed & Developed by]
-        :blue[Wenkang Li & Gunther Hua]'''
-         )
+        st.write(':grey[Designed & Developed by] :blue[李文康]')
     left_co, cent_co,last_co = st.columns([0.22,0.6,0.18])
     with cent_co:
         st.write(':grey[Powered by] **Vertex AI**')
 
-# define text generation function
+# 定义生成文本的函数
 def generate_text(prompt):
     responses = model.generate_content(
         [prompt],
@@ -158,7 +159,7 @@ def generate_text(prompt):
 
     return generated_text
 
-#define video understanding function
+#定义视频理解的函数
 def generate_video_text(prompt):
     video_responses = model.generate_content(
         [media, prompt],
@@ -173,6 +174,7 @@ def generate_video_text(prompt):
 
     return generated_video_text
 
+# 定义生成模型参数
 generation_config = {
     "max_output_tokens": 8192,
     "temperature": temperature,
@@ -186,34 +188,31 @@ safety_settings = {
     generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_ONLY_HIGH,
 }
 
-#continue streamlit UI
-gcs_file = st.text_input("Please input your Google Cloud Storage uri", placeholder='gs://"bucket name"/"file name"')
+#继续streamlit界面
+gcs_file = st.text_input("请输入您的文件的GCS链接", placeholder='gs://"您的存储桶名字"/"您的文件名"')
 
+#定义全局可用
 media_mime_type = None
 
 file_extension = None
 
-# if no file uploaded, get file type according to GCS uri
+# 如果没有上传文件，则根据 GCS 链接推断文件类型
 if gcs_file:
     file_extension = gcs_file.split(".")[-1].lower()
     if file_extension == "mp4":
         media_mime_type = "video/mp4"
     elif file_extension == "jpg":
         media_mime_type = "image/jpeg"
-    elif file_extension == "jpeg":
-        media_mime_type = "image/jpeg"
     elif file_extension == "png":
         media_mime_type = "image/png"
     elif file_extension == "gif":
         media_mime_type = "video/wmv"
 
-# if file uploaded, get file type according to file name
+# 如果上传了文件，则使用上传文件的文件类型
 elif file_type:
     if file_type == "mp4":
         media_mime_type = "video/mp4"
     elif file_type == "jpg":
-        media_mime_type = "image/jpeg"
-    elif file_type == "jpeg":
         media_mime_type = "image/jpeg"
     elif file_type == "png":
         media_mime_type = "image/png"
@@ -226,41 +225,41 @@ media = Part.from_uri(
     uri=gcs_file
 )
 
-prompt = st.text_area("Please input your prompt：", "")
+prompt = st.text_area("请输入您的提示词：", "")
 
 with st.form("myform"):
-    left_co, cent_co,last_co = st.columns([0.35,0.35,0.3])
+    left_co, cent_co,last_co = st.columns([0.42,0.29,0.29])
     with cent_co:
-        submitted = st.form_submit_button("Generate Text Content")
+        submitted = st.form_submit_button("生成文本")
     if gcs_file and submitted and not prompt:
-        st.info("Please input prompt")
+        st.info("请输入提示词")
     
     if not gcs_file and prompt and submitted:
-        with st.spinner('A second please :coffee: the content is upcoming...'):
+        with st.spinner('请稍等 :coffee: 马上就来...'):
             generated_text = generate_text(prompt)
             st.write(generated_text)
         
     if prompt and submitted and gcs_file:
-        prompt_with_video = f"document content: \n{media}\n\n prompt: \n{prompt}\n\n answer："
-        with st.spinner('A second please :coffee: the content is upcoming...'):
+        prompt_with_video = f"文档内容: \n{media}\n\n 提示词: \n{prompt}\n\n回答："
+        with st.spinner('请稍等 :coffee: 马上就来...'):
             generated_video_text_text = generate_video_text(prompt_with_video)
             
             if file_extension in ("jpg", "png"):
-                # get bucket name and blob name from GCS uri
+                # 从 GCS 链接中提取存储桶名称和 blob 名称
                 bucket_name = gcs_file.split("/")[2]
                 blob_name = "/".join(gcs_file.split("/")[3:])
             
-                # get GCS client and bucket
+                # 获取 GCS 客户端和存储桶
                 storage_client = storage.Client()
                 bucket = storage_client.bucket(bucket_name)
                 
-                # get blob object
+                # 获取 blob 对象
                 blob = bucket.blob(blob_name)
                 
-                # get https:// uri thru blob.public.url
+                # 使用 blob.public_url 获取 https:// 链接
                 public_url = blob.public_url
             
-                # display the image with public_url
+                # 使用 https:// 链接显示图片
                 st.image(public_url, caption=blob_name)
             
             st.write(generated_video_text_text)

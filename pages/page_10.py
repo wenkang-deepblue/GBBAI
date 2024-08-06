f732b97eea465036532d11ba661625bf491966c0
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-08-06 18:00:02 +0800

    Update page_10.py

--- pages/page_10.py
+++ pages/page_10.py
@@ -2,16 +2,93 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # Streamlit UI
-left_co, cent_co,last_co = st.columns([0.39,0.31,0.30])
+left_left_co, cent_co,last_co = st.columns([0.39,0.31,0.30])
 with cent_co:
     st.title(":blue[GBB] :rainbow[AI]")
-left_co, cent_co,last_co = st.columns([0.27,0.53,0.1])
+left_co, cent_co,last_co = st.columns([0.38,0.32,0.3])
 with cent_co:
-    st.caption(":blue[_Enterprise Customer Service Chatbot Platform_]")
-st.image('https://storage.googleapis.com/ghackathon/page_7.png')
+    st.caption(":blue[_企业级客服机器人平台_]")
+st.image('https://storage.googleapis.com/ghackathon/page_15_zh.png')
 left_co, cent_co,last_co = st.columns([0.24,0.51,0.25])
 with cent_co:
     st.subheader('', divider='rainbow')
+    
+with st.sidebar:
+        left_co, cent_co,last_co = st.columns([0.34,0.33,0.33])
+        with cent_co:
+                st.image('https://storage.googleapis.com/ghackathon/image2.gif')
+        left_co, cent_co,last_co = st.columns([0.36,0.32,0.32])
+        with cent_co:
+                st.title(":blue[GBB] :rainbow[AI]")
+        st.page_link("homepage.py", label="主页", icon="🏠")
+        st.page_link("pages/page_0.py", label="文本生成", icon="📖")
+        st.page_link("pages/page_9.py", label="视频理解", icon="🎞️")
+        st.page_link("pages/page_13.py", label="文本翻译", icon="🇺🇳")
+        st.page_link("pages/page_2.py", label="RAG搜索", icon="🔍")
+        st.page_link("pages/page_3.py", label="媒体搜索", icon="🎥")
+        st.page_link("pages/page_16.py", label="图片生成", icon="🎨")
+        st.page_link("pages/page_18.py", label="聊天机器人", icon="💬")
+        custom_page_link("pages/page_15.py", label="游戏客服平台", icon="🤖", new_tab=True)
+        st.page_link("pages/page_21.py", label="电商客服平台", icon="🤖")
+        st.page_link("pages/page_19.py", label="Claude3.5聊天机器人", icon="💬")
+        st.page_link("pages/page_23.py", label="Llama3.1聊天机器人", icon="💬")
+        st.page_link("https://pantheon.corp.google.com/translation/hub", label="GCP翻译门户", icon="🌎")
+        st.page_link("https://pantheon.corp.google.com/vertex-ai/generative/multimodal/gallery", label="GCP控制台 - Gemini", icon="🌎")
+        st.page_link("https://pantheon.corp.google.com/gen-app-builder/engines", label="GCP控制台 - App Builder", icon="🌎")
+        st.text("")
+        st.subheader('', divider='rainbow')
+        st.text("")
+        st.markdown(
+                """
+        ## 关于
+        这是由:blue[Google Cloud Vertex AI]驱动的生成式AI平台以及企业级RAG搜索引擎
+        - [:cloud: Google Cloud Vertex AI](https://cloud.google.com/vertex-ai?hl=en)
+
+        """
+        )
+        st.text("")
+        st.text("")
+        st.text("")
+        st.text("")
+        st.text("")
+        left_co, cent_co,last_co = st.columns([0.39,0.31,0.30])
+        with cent_co:
+                st.write('© GBB')
+        left_co, cent_co,last_co = st.columns([0.09,0.83,0.08])
+        with cent_co:
+            st.write(':grey[Designed & Developed by] :blue[李文康]')
+        left_co, cent_co,last_co = st.columns([0.22,0.6,0.18])
+        with cent_co:
+                st.write(':grey[Powered by] **Vertex AI**')
+
+def custom_page_link(url, label, icon, new_tab=False):
+    corrected_url = url.replace("pages/", "").replace(".py", "")
+    
+    if new_tab:
+        st.markdown(
+            f'''
+            <div class="row-widget stPageLink" style="width: 282px; margin: -0.4rem 0 0.cbec99246ab9ab5rem 0;">
+                <div class="st-emotion-cache-j7qwjs e11k5jya2" style="padding: 0;">
+                    <a href="/{corrected_url}" target="_blank" rel="noopener noreferrer" 
+                       class="st-emotion-cache-n7e918 e11k5jya1" style="display: flex; align-items: center;">
+                        <span color="#31333F" class="st-emotion-cache-6jwljf eyeqlp52" style="margin-right: 0rem;">
+                            <span data-testid="stIconEmoji" aria-hidden="true" 
+                                  class="st-emotion-cache-8hkptd eyeqlp50">{icon}</span>
+                        </span>
+                        <span class="st-emotion-cache-pkbazv e11k5jya0">
+                            <div data-testid="stMarkdownContainer" 
+                                 class="st-emotion-cache-187vdiz e1nzilvr4">
+                                <p style="margin: 0;">{label}</p>
+                            </div>
+                        </span>
+                    </a>
+                </div>
+            </div>
+            ''',
+            unsafe_allow_html=True
+        )
+    else:
+        st.page_link(url, label=label, icon=icon)
 
 # Embed Dialogflow code within an HTML component
 components.html("""
@@ -62,50 +139,3 @@ components.html("""
     }
 </style>
 """, height=600) # Adjust height as needed
-
-with st.sidebar:
-        left_co, cent_co,last_co = st.columns([0.34,0.33,0.33])
-        with cent_co:
-                st.image('https://storage.googleapis.com/ghackathon/image2.gif')
-        left_co, cent_co,last_co = st.columns([0.36,0.32,0.32])
-        with cent_co:
-                st.title(":blue[GBB] :rainbow[AI]")
-        st.page_link("homepage.py", label="Homepage", icon="🏠")
-        st.page_link("pages/page_1.py", label="Article Generation", icon="📖")
-        st.page_link("pages/page_2.py", label="Media Comprehension", icon="🎞️")
-        st.page_link("pages/page_3.py", label="Text Translation", icon="🇺🇳")
-        st.page_link("pages/page_4.py", label="Document Search", icon="🔍")
-        st.page_link("pages/page_5.py", label="Media Search", icon="🎥")
-        st.page_link("pages/page_6.py", label="Image Generation", icon="🎨")
-        st.page_link("pages/page_7.py", label="Customer Service Chatbot", icon="🤖")
-        st.page_link("https://pantheon.corp.google.com/vertex-ai/generative/multimodal/create/text?project=lwk-genai-test", label="GCP Console - Gemini", icon="🌎")
-        st.page_link("https://pantheon.corp.google.com/gen-app-builder/locations/global/engines/lwk-rag-search_1713579191717/preview/search?e=13803378&mods=dm_deploy_from_gcs&project=lwk-genai-test", label="GCP Console - Vertex AI Searh", icon="🌎")
-        st.text("")
-        st.subheader('', divider='rainbow')
-        st.text("")
-        st.markdown(
-            """
-        ## About
-        This is an enterprise readiness GenAI platform powered by :blue[Google Cloud Vertex AI]
-        - [:cloud: Google Cloud Vertex AI](https://cloud.google.com/vertex-ai?hl=en)
-    
-        """
-        )
-        st.text("")
-        st.text("")
-        st.text("")
-        st.text("")
-        st.text("")
-        left_co, cent_co,last_co = st.columns([0.3,0.4,0.30])
-        with cent_co:
-            st.write('© [Wenkang Li](https://moma.corp.google.com/person/wenkangli?q=image%20generatioin%20streamlit)')
-        left_co, cent_co,last_co = st.columns([0.2,0.79,0.1])
-        with cent_co:
-            st.write(
-            '''
-            :grey[Designed & Developed by]
-            :blue[Wenkang Li & Gunther Hua]'''
-             )
-        left_co, cent_co,last_co = st.columns([0.22,0.6,0.18])
-        with cent_co:
-            st.write(':grey[Powered by] **Vertex AI**')

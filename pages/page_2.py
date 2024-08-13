d5cb7786ae364608182b77861913dc7890175131
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-08-13 21:09:30 +0800

    更新 page_2.py

--- pages/page_2.py
+++ pages/page_2.py
@@ -24,7 +24,7 @@ model = GenerativeModel("gemini-1.5-flash-001")
 # Streamlit 应用界面
 left_co, cent_co,last_co = st.columns([0.39,0.31,0.30])
 with cent_co:
-    st.title(":blue[GBB] :rainbow[AI]")
+    st.title(":blue[GCP Gen]:rainbow[AI]")
 left_co, cent_co,last_co = st.columns([0.39,0.31,0.3])
 with cent_co:
     st.caption(":blue[_企业级内容生成平台_]")
@@ -133,7 +133,7 @@ with st.sidebar:
     
     left_co, cent_co,last_co = st.columns([0.39,0.31,0.30])
     with cent_co:
-        st.write('© GBB')
+        st.write('© LWK')
     left_co, cent_co,last_co = st.columns([0.09,0.83,0.08])
     with cent_co:
         st.markdown(

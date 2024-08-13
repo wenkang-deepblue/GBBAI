a9a1297df7f460804e7e773aaadd4fdb065bc043
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-08-13 21:11:04 +0800

    更新 page_4.py

--- pages/page_4.py
+++ pages/page_4.py
@@ -29,7 +29,7 @@ headers = {
 # streamlit界面
 left_co, cent_co,last_co = st.columns([0.39,0.31,0.30])
 with cent_co:
-    st.title(":blue[GBB] :rainbow[AI]")
+    st.title(":blue[GCP Gen]:rainbow[AI]")
 left_co, cent_co,last_co = st.columns([0.39,0.31,0.3])
 with cent_co:
     st.caption(":blue[_企业级RAG搜索引擎_]")
@@ -80,7 +80,7 @@ with st.sidebar:
     st.text("")
     left_co, cent_co,last_co = st.columns([0.39,0.31,0.30])
     with cent_co:
-        st.write('© GBB')
+        st.write('© LWK')
     left_co, cent_co,last_co = st.columns([0.09,0.83,0.08])
     with cent_co:
         st.markdown(

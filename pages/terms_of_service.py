937383225a5b2ed1590ed26c865f5ddf18429951
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-08-15 12:35:38 +0800

    Update terms_of_service.py

--- pages/terms_of_service.py
+++ pages/terms_of_service.py
@@ -1,6 +1,58 @@
 import streamlit as st
 
-st.page_link("homepage.py", label="返回首页", icon="🏠")
+with st.sidebar:
+    left_co, cent_co,last_co = st.columns([0.34,0.33,0.33])
+    with cent_co:
+            st.image('https://storage.googleapis.com/ghackathon/image2.gif')
+    left_co, cent_co,last_co = st.columns([0.36,0.32,0.32])
+    with cent_co:
+            st.title(":blue[GCP Gen]:rainbow[AI]")
+    st.page_link("homepage.py", label="主页", icon="🏠")
+    st.page_link("pages/page_1.py", label="文本生成", icon="📖")
+    st.page_link("pages/page_2.py", label="视频理解", icon="🎞️")
+    st.page_link("pages/page_3.py", label="文本翻译", icon="🇺🇳")
+    st.page_link("pages/page_12.py", label="旅游顾问", icon="✈️")
+    st.page_link("pages/page_4.py", label="RAG搜索", icon="🔍")
+    st.page_link("pages/page_5.py", label="媒体搜索", icon="🎥")
+    st.page_link("pages/page_6.py", label="图片生成", icon="🎨")
+    st.page_link("pages/page_7.py", label="聊天机器人", icon="💬")
+    st.page_link("pages/page_8.py", label="游戏客服平台", icon="🤖")
+    st.page_link("pages/page_9.py", label="电商客服平台", icon="🤖")
+    st.page_link("pages/page_10.py", label="Claude3.5聊天机器人", icon="💬")
+    st.page_link("pages/page_11.py", label="Llama3.1聊天机器人", icon="💬")
+    st.page_link("https://pantheon.corp.google.com/translation/hub", label="GCP翻译门户", icon="🌎")
+    st.page_link("https://pantheon.corp.google.com/vertex-ai/generative/multimodal/gallery", label="GCP控制台 - Gemini", icon="🌎")
+    st.page_link("https://pantheon.corp.google.com/gen-app-builder/engines", label="GCP控制台 - App Builder", icon="🌎")
+    st.text("")
+    st.subheader('', divider='rainbow')
+    st.text("")
+    st.markdown(
+        """
+    ## 关于
+    这是由:blue[Google Cloud Vertex AI]驱动的生成式AI平台以及企业级RAG搜索引擎
+        """
+    )
+    st.page_link("https://cloud.google.com/vertex-ai?hl=en", label="Google Cloud Vertex AI", icon="☁️")
+    
+    left_co, cent_co,last_co = st.columns([0.41,0.31,0.28])
+    with cent_co:
+            st.write('© LWK')
+    left_co, cent_co,last_co = st.columns([0.09,0.83,0.08])
+    with cent_co:
+        st.markdown(
+        f'<p style="text-align: center;">'
+        f'<span style="color: grey;">Designed & Developed by</span> '
+        f'<a href="{st.secrets["developer_profile_link"]}" '
+        f'style="color: #185ABC; text-decoration: underline;" target="_blank">{st.secrets["developer_name"]}</a>'
+        f'</p>',
+        unsafe_allow_html=True
+    )
+    left_co, cent_co,last_co = st.columns([0.22,0.6,0.18])
+    with cent_co:
+        st.write(':grey[Powered by] **Vertex AI**')
+
+    st.page_link("pages/terms_of_service.py", label="用户服务协议", icon="📄")
+    st.page_link("pages/privacy_policy.py", label="隐私政策", icon="🔒")
 
 left_co, cent_co,last_co = st.columns([0.15,0.7,0.15])
 with cent_co:

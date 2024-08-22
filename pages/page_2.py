5fbab57ded8164f672ba452b820cb09605833e37
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-08-22 15:51:05 +0800

    Update page_2.py

--- pages/page_2.py
+++ pages/page_2.py
@@ -20,7 +20,7 @@ with st.sidebar:
     st.markdown(f"""
         <div style="background-color: #d4edda; border-color: #c3e6cb; color: #155724; 
                     padding: 10px; border-radius: 0.25rem; text-align: center; margin-bottom: 10px;">
-            <p style="margin-bottom: 0;">欢迎, {st.session_state.user_email}!</p>
+            <p style="margin-bottom: 0;">欢迎!</p>
         </div>
     """, unsafe_allow_html=True)
     left_co, cent_co,last_co = st.columns([0.35,0.33,0.32])
@@ -41,7 +41,7 @@ creds.refresh(auth_req)
 vertexai.init(project="lwk-genai-test", location="us-central1", credentials=creds)
 model = GenerativeModel("gemini-1.5-flash-001")
 # Streamlit 应用界面
-left_co, cent_co,last_co = st.columns([0.39,0.31,0.30])
+left_co, cent_co,last_co = st.columns([0.3,0.5,0.2])
 with cent_co:
     st.title(":blue[GCP Gen]:rainbow[AI]")
 left_co, cent_co,last_co = st.columns([0.39,0.31,0.3])

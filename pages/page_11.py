220d4aed973ab4957449da074a59905d5af84fd6
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-08-22 15:46:46 +0800

    Update page_11.py

--- pages/page_11.py
+++ pages/page_11.py
@@ -23,7 +23,7 @@ with st.sidebar:
     st.markdown(f"""
         <div style="background-color: #d4edda; border-color: #c3e6cb; color: #155724; 
                     padding: 10px; border-radius: 0.25rem; text-align: center; margin-bottom: 10px;">
-            <p style="margin-bottom: 0;">欢迎, {st.session_state.user_email}!</p>
+            <p style="margin-bottom: 0;">欢迎!</p>
         </div>
     """, unsafe_allow_html=True)
     left_co, cent_co,last_co = st.columns([0.35,0.33,0.32])
@@ -70,7 +70,7 @@ def load_gif(gif_url):
 thinking_gif = load_gif("https://storage.googleapis.com/ghackathon/typing-dots-40.gif")
 
 # Streamlit应用界面
-left_co, cent_co,last_co = st.columns([0.39,0.31,0.30])
+left_co, cent_co,last_co = st.columns([0.3,0.5,0.2])
 with cent_co:
     st.title(":blue[GCP Gen]:rainbow[AI]")
 left_co, cent_co,last_co = st.columns([0.43,0.37,0.3])

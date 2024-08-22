5c52eddc0c09453c6b2dde6729796cd2491581c8
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-08-22 15:53:28 +0800

    Update page_8.py

--- pages/page_8.py
+++ pages/page_8.py
@@ -12,7 +12,7 @@ with st.sidebar:
     st.markdown(f"""
         <div style="background-color: #d4edda; border-color: #c3e6cb; color: #155724; 
                     padding: 10px; border-radius: 0.25rem; text-align: center; margin-bottom: 10px;">
-            <p style="margin-bottom: 0;">欢迎, {st.session_state.user_email}!</p>
+            <p style="margin-bottom: 0;">欢迎!</p>
         </div>
     """, unsafe_allow_html=True)
     left_co, cent_co,last_co = st.columns([0.35,0.33,0.32])
@@ -49,7 +49,7 @@ def custom_page_link(url, label, icon, new_tab=False):
         st.page_link(url, label=label, icon=icon)
 
 # Streamlit 应用界面
-left_co, cent_co,last_co = st.columns([0.39,0.31,0.30])
+left_co, cent_co,last_co = st.columns([0.3,0.5,0.2])
 with cent_co:
     st.title(":blue[GCP Gen]:rainbow[AI]")
 left_co, cent_co,last_co = st.columns([0.38,0.32,0.3])

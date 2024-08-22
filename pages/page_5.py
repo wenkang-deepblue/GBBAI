91682078b3acb32a8d8a6ec147b0c1e20b50ffb7
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-08-22 15:52:16 +0800

    Update page_5.py

--- pages/page_5.py
+++ pages/page_5.py
@@ -16,7 +16,7 @@ with st.sidebar:
     st.markdown(f"""
         <div style="background-color: #d4edda; border-color: #c3e6cb; color: #155724; 
                     padding: 10px; border-radius: 0.25rem; text-align: center; margin-bottom: 10px;">
-            <p style="margin-bottom: 0;">欢迎, {st.session_state.user_email}!</p>
+            <p style="margin-bottom: 0;">欢迎!</p>
         </div>
     """, unsafe_allow_html=True)
     left_co, cent_co,last_co = st.columns([0.35,0.33,0.32])
@@ -42,7 +42,7 @@ headers = {
     "Authorization": "Bearer {}".format(creds.token),
 }
 
-left_co, cent_co,last_co = st.columns([0.39,0.31,0.30])
+left_co, cent_co,last_co = st.columns([0.3,0.5,0.2])
 with cent_co:
     st.title(":blue[GCP Gen]:rainbow[AI]")
 left_co, cent_co,last_co = st.columns([0.39,0.31,0.3])

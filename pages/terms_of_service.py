17cd2c711aed9ff6ecb43c94b23a4eb77c9e8377
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-08-24 16:55:56 +0800

    Update terms_of_service.py

--- pages/terms_of_service.py
+++ pages/terms_of_service.py
@@ -4,7 +4,7 @@ with st.sidebar:
     left_co, cent_co,last_co = st.columns([0.34,0.33,0.33])
     with cent_co:
             st.image('https://storage.googleapis.com/ghackathon/image2.gif')
-    left_co, cent_co,last_co = st.columns([0.36,0.32,0.32])
+    left_co, cent_co,last_co = st.columns([0.28,0.5,0.22])
     with cent_co:
             st.title(":blue[GCP Gen]:rainbow[AI]")
     st.page_link("homepage.py", label="主页", icon="🏠")

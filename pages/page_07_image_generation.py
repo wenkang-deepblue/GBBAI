4aa9d0b8e666fbb2980511ff39664aa7df1fc9e8
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-08-24 17:03:36 +0800

    Update page_07_image_generation.py

--- pages/page_07_image_generation.py
+++ pages/page_07_image_generation.py
@@ -110,7 +110,7 @@ with st.sidebar:
         st.write(':grey[Powered by] **Vertex AI**')
 
     st.page_link("pages/terms_of_service.py", label="用户服务协议", icon="📄")
-    st.page_link("pages/privacy_policy.py", label="隐私政策", icon="🔒")
+    st.page_link("pages/privacy_policy.py", label="用户隐私政策", icon="🔒")
     
 prompt = st.text_area("请输入您的提示词：", "")
 

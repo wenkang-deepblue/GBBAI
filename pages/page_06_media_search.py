6d4d60b1fd3fe25c8b8a050a559a3fa2ae8a46db
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-08-30 22:18:40 +0800

    Update page_06_media_search.py

--- pages/page_06_media_search.py
+++ pages/page_06_media_search.py
@@ -45,7 +45,7 @@ headers = {
 left_co, cent_co,last_co = st.columns([0.35,0.35,0.3])
 with cent_co:
     st.title(":blue[GCP Gen]:rainbow[AI]")
-left_co, cent_co,last_co = st.columns([0.35,0.37,0.28])
+left_co, cent_co,last_co = st.columns([0.34,0.38,0.28])
 with cent_co:
     st.caption(":blue[_Enterprise-ready Media Search Engine_]")
 st.image('https://storage.googleapis.com/ghackathon/page_5.png')

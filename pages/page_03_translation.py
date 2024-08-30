cc70c9d2db90de6299c9f625a8f9f1c61609e7ce
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-08-30 21:28:12 +0800

    Update page_03_translation.py

--- pages/page_03_translation.py
+++ pages/page_03_translation.py
@@ -18,7 +18,7 @@ with st.sidebar:
     st.markdown(f"""
         <div style="background-color: #d4edda; border-color: #c3e6cb; color: #155724; 
                     padding: 10px; border-radius: 0.25rem; text-align: center; margin-bottom: 10px;">
-            <p style="margin-bottom: 0;">欢迎!</p>
+            <p style="margin-bottom: 0;">Welcome!</p>
         </div>
     """, unsafe_allow_html=True)
     left_co, cent_co,last_co = st.columns([0.35,0.33,0.32])
@@ -38,22 +38,22 @@ creds.refresh(auth_req)
 
 vertexai.init(project="lwk-genai-test", location="us-central1", credentials=creds)
 
-# Streamlit 应用界面
+# Streamlit application interface
 left_co, cent_co,last_co = st.columns([0.35,0.35,0.3])
 with cent_co:
     st.title(":blue[GCP Gen]:rainbow[AI]")
 left_co, cent_co,last_co = st.columns([0.42,0.30,0.28])
 with cent_co:
-    st.caption(":blue[_企业级翻译平台_]")
+    st.caption(":blue[_Enterprise-ready Translation Platform_]")
 st.image('https://storage.googleapis.com/ghackathon/page_4_zh.png')
 left_co, cent_co,last_co = st.columns([0.24,0.51,0.25])
 with cent_co:
     st.subheader('', divider='rainbow')
     
-#定义translation_option为全局变量
+# Define translation_option as a global variable
 translation_option = None
     
-#继续streamlit sidebar界面
+# Continue streamlit sidebar interface
 with st.sidebar:
     left_co, cent_co,last_co = st.columns([0.34,0.33,0.33])
     with cent_co:
@@ -61,49 +61,49 @@ with st.sidebar:
     left_co, cent_co,last_co = st.columns([0.28,0.5,0.22])
     with cent_co:
         st.title(":blue[GCP Gen]:rainbow[AI]")
-    temperature = st.slider("调整模型Temperature", min_value=0.0, max_value=2.0, value=1.0, help=(
+    temperature = st.slider("Adjust Model Temperature", min_value=0.0, max_value=2.0, value=1.0, help=(
         """
-        Temperature用于响应生成期间的采样，这发生在应用 topP 和 topK 时。Temperature控制了token选择中的随机程度。对于需要较少开放式或创造性响应的提示，较低的temperature是好的，而较高的temperature可以导致更多样化或创造性的结果。Temperature为 0 意味着始终选择最高概率的token。在这种情况下，给定提示的响应大多是确定的，但仍有可能出现少量变化。
+        Temperature is used for sampling during response generation, which occurs when applying topP and topK. Temperature controls the degree of randomness in token selection. For prompts that require less open-ended or creative responses, a lower temperature is good, while a higher temperature can lead to more diverse or creative results. A temperature of 0 means always selecting the highest probability token. In this case, the response for a given prompt is mostly deterministic, but there may still be some variation.
         
-        如果模型返回的响应过于通用、太短或模型给出回退响应，请尝试提高temperature。
+        If the model returns responses that are too generic, too short, or the model gives fallback responses, try increasing the temperature.
         """
     ))
-    top_p = st.slider ("调整模型Top_p", min_value=0.00, max_value=1.00, value=0.95, help=(
+    top_p = st.slider ("Adjust Model Top_p", min_value=0.00, max_value=1.00, value=0.95, help=(
         """
-        Top-P 改变了模型选择输出tokens的方式。Tokens按照从最可能（见top-K）到最不可能的顺序进行选择，直到它们的概率之和等于top-P值。例如，如果token A、B和C的概率分别为0.3、0.2和0.1，top-P值为0.5，那么模型将使用温度从A或B中选择下一个token，并排除C作为候选。
+        Top-P changes how the model selects output tokens. Tokens are chosen from most likely to least likely (see top-K) until the sum of their probabilities equals the top-P value. For example, if tokens A, B, and C have probabilities 0.3, 0.2, and 0.1 respectively, and the top-P value is 0.5, the model will select the next token from A or B using temperature and discard C as a candidate.
 
-        指定较低的值会得到较少的随机响应，指定较高的值会得到更多的随机响应。
+        Specifying a lower value will result in less random responses, while specifying a higher value will result in more random responses.
         """
     ))
     st.subheader('',divider='rainbow')
     translation_option = st.selectbox(
-    "请选择翻译的目标语言：",
-    ("英文", "日文", "韩文", "德文", "法文", "西班牙文", "葡萄牙文", "意大利文", "阿拉伯文", "中文"),
+    "Please select the target language for translation:",
+    ("German", "French", "Spanish", "Portuguese", "Italian", "Russian", "Arabic", "Chinese", "Japanese", "Korean", "English"),
     index=None,
-    placeholder="请选择语言")
-    st.page_link("homepage.py", label="主页", icon="🏠")
-    st.page_link("pages/page_01_text_generation.py", label="文本生成", icon="📖")
-    st.page_link("pages/page_02_media_understanding.py", label="视频理解", icon="🎞️")
-    st.page_link("pages/page_03_translation.py", label="文本翻译", icon="🇺🇳")
-    st.page_link("pages/page_04_travel_advisor.py", label="旅游顾问", icon="✈️")
-    st.page_link("pages/page_05_rag_search.py", label="RAG搜索", icon="🔍")
-    st.page_link("pages/page_06_media_search.py", label="媒体搜索", icon="🎥")
-    st.page_link("pages/page_07_image_generation.py", label="图片生成", icon="🎨")
-    st.page_link("pages/page_08_chatbot.py", label="聊天机器人", icon="💬")
-    st.page_link("pages/page_09_gaming_servicebot.py", label="游戏客服平台", icon="🤖")
-    st.page_link("pages/page_10_ecommerce_servicebot.py", label="电商客服平台", icon="🤖")
-    st.page_link("pages/page_11_claude_chatbot.py", label="Claude3.5聊天机器人", icon="💬")
-    st.page_link("pages/page_12_llama_chatbot.py", label="Llama3.1聊天机器人", icon="💬")
-    st.page_link("https://pantheon.corp.google.com/translation/hub", label="GCP翻译门户", icon="🌎")
-    st.page_link("https://pantheon.corp.google.com/vertex-ai/generative/multimodal/gallery", label="GCP控制台 - Gemini", icon="🌎")
-    st.page_link("https://pantheon.corp.google.com/gen-app-builder/engines", label="GCP控制台 - App Builder", icon="🌎")
+    placeholder="Please select a language")
+    st.page_link("homepage.py", label="Home", icon="🏠")
+    st.page_link("pages/page_01_text_generation.py", label="Text Generation", icon="📖")
+    st.page_link("pages/page_02_media_understanding.py", label="Media Understanding", icon="🎞️")
+    st.page_link("pages/page_03_translation.py", label="Text Translation", icon="🇺🇳")
+    st.page_link("pages/page_04_travel_advisor.py", label="Travel Advisor", icon="✈️")
+    st.page_link("pages/page_05_rag_search.py", label="RAG Search", icon="🔍")
+    st.page_link("pages/page_06_media_search.py", label="Media Search", icon="🎥")
+    st.page_link("pages/page_07_image_generation.py", label="Image Generation", icon="🎨")
+    st.page_link("pages/page_08_chatbot.py", label="Chatbot", icon="💬")
+    st.page_link("pages/page_09_gaming_servicebot.py", label="Gaming Servicebot", icon="🤖")
+    st.page_link("pages/page_10_ecommerce_servicebot.py", label="E-commerce Servicebot", icon="🤖")
+    st.page_link("pages/page_11_claude_chatbot.py", label="Claude 3.5 Chatbot", icon="💬")
+    st.page_link("pages/page_12_llama_chatbot.py", label="Llama 3.1 Chatbot", icon="💬")
+    st.page_link("https://pantheon.corp.google.com/translation/hub", label="GCP Translation Hub", icon="🌎")
+    st.page_link("https://pantheon.corp.google.com/vertex-ai/generative/multimodal/gallery", label="GCP Console - Gemini", icon="🌎")
+    st.page_link("https://pantheon.corp.google.com/gen-app-builder/engines", label="GCP Console - App Builder", icon="🌎")
     st.text("")
     st.subheader('', divider='rainbow')
     st.text("")
     st.markdown(
         """
-    ## 关于
-    这是由:blue[Google Cloud Vertex AI]驱动的生成式AI平台以及企业级RAG搜索引擎
+    ## About
+    This is a generative AI platform powered by :blue[Google Cloud Vertex AI] and an enterprise-ready RAG search engine
         """
     )
     st.page_link("https://cloud.google.com/vertex-ai?hl=en", label="Google Cloud Vertex AI", icon="☁️")
@@ -124,10 +124,10 @@ with st.sidebar:
     with cent_co:
         st.write(':grey[Powered by] **Vertex AI**')
 
-    st.page_link("pages/terms_of_service.py", label="用户服务协议", icon="📄")
-    st.page_link("pages/privacy_policy.py", label="用户隐私政策", icon="🔒")
+    st.page_link("pages/terms_of_service.py", label="Terms of Service", icon="📄")
+    st.page_link("pages/privacy_policy.py", label="Privacy Policy", icon="🔒")
 
-# 定义生成文本的函数
+# Define the function to generate text
 def generate_text(prompt):
   vertexai.init(project="lwk-genai-test", location="us-central1")
   model = GenerativeModel("gemini-1.5-flash-001")
@@ -144,7 +144,7 @@ def generate_text(prompt):
 
   return generated_text
 
-# 定义生成模型参数
+# Define generation model parameters
 generation_config = {
     "max_output_tokens": 8192,
     "temperature": temperature,
@@ -158,12 +158,12 @@ safety_settings = {
     generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_ONLY_HIGH,
 }
 
-#继续streamlit界面
-st.write("请注意：为了方便您区分翻译结果，请只选择“上传文档”或“输入文字”。")
+# Continue streamlit interface
+st.write("Please note: To help you distinguish the translation results, please choose either 'Upload Document' or 'Input Text'.")
 
-uploaded_files = st.file_uploader("请上传您需要翻译的文档，可以同时选择多份文档上传：", type=("txt"), accept_multiple_files=True)
+uploaded_files = st.file_uploader("Please upload the documents you need to translate. You can select multiple documents to upload:", type=("txt"), accept_multiple_files=True)
 
-prompt = st.text_area("或者输入您需要翻译的文字：", "")
+prompt = st.text_area("Or input the text you need to translate:", "")
 
 if uploaded_files:
     all_text = ""
@@ -176,27 +176,27 @@ if uploaded_files:
 with st.form("myform"):
     left_co, cent_co,last_co = st.columns([0.42,0.29,0.29])
     with cent_co:
-        submitted = st.form_submit_button("开始翻译")
+        submitted = st.form_submit_button("Start Translation")
     if uploaded_files and prompt and translation_option and submitted:
-        st.error("为了方便您区分翻译结果，请只选择“上传文档”或“输入文字”。")
+        st.error("To help you distinguish the translation results, please choose either 'Upload Document' or 'Input Text'.")
         
     if uploaded_files and prompt and submitted:
-        st.error("为了方便您区分翻译结果，请只选择“上传文档”或“输入文字”。")
+        st.error("To help you distinguish the translation results, please choose either 'Upload Document' or 'Input Text'.")
         
     if uploaded_files and submitted and not prompt and not translation_option:
-        st.error("👈 请选择您需要翻译的语言。")
+        st.error("👈 Please select the language you want to translate to.")
         
     if prompt and submitted and not uploaded_files and not translation_option:
-        st.error("👈 请选择您需要翻译的语言。")
+        st.error("👈 Please select the language you want to translate to.")
     
     if prompt and translation_option and submitted and not uploaded_files:
-        prompt_without_article = f"请将下面的这段文字翻译成{translation_option},请直接输出翻译结果\n\n 需要翻译的文字: \n{prompt}\n\n回答："
-        with st.spinner('请稍等 :coffee: 马上就好...'):
+        prompt_without_article = f"Please translate the following text into {translation_option}, and output the translation result directly\n\n Text to be translated: \n{prompt}\n\nAnswer:"
+        with st.spinner('Please wait :coffee: Almost done...'):
             generated_text = generate_text(prompt_without_article)
             st.write(generated_text)
             
     if uploaded_files and translation_option and submitted and not prompt:
-        prompt_with_article = f"请将下面的这段文字翻译成{translation_option},请直接输出翻译结果\n\n 需要翻译的文字: \n{all_text}\n\n回答："
-        with st.spinner('请稍等 :coffee: 马上就好...'):
+        prompt_with_article = f"Please translate the following text into {translation_option}, and output the translation result directly\n\n Text to be translated: \n{all_text}\n\nAnswer:"
+        with st.spinner('Please wait :coffee: Almost done...'):
             generated_text = generate_text(prompt_with_article)
             st.write(generated_text)

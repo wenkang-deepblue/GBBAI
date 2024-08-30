cffbce10d48a422a7f950737d0f02253f099e539
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-08-30 21:46:14 +0800

    Update page_02_media_understanding.py

--- pages/page_02_media_understanding.py
+++ pages/page_02_media_understanding.py
@@ -107,7 +107,7 @@ with st.sidebar:
         return f"gs://{bucket_name}/{destination_blob_name}"
           
     with st.form("mysidebarform"):
-        left_co, cent_co,last_co = st.columns([0.3,0.4,0.3])
+        left_co, cent_co,last_co = st.columns([0.32,0.4,0.28])
         with cent_co:
             submitted = st.form_submit_button("Upload")
         

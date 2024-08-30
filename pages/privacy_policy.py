d27bfd33c74d6ef7999c0faaff222933f22cf55b
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-08-30 21:30:33 +0800

    Update privacy_policy.py

--- pages/privacy_policy.py
+++ pages/privacy_policy.py
@@ -7,29 +7,29 @@ with st.sidebar:
     left_co, cent_co,last_co = st.columns([0.28,0.5,0.22])
     with cent_co:
             st.title(":blue[GCP Gen]:rainbow[AI]")
-    st.page_link("homepage.py", label="主页", icon="🏠")
-    st.page_link("pages/page_01_text_generation.py", label="文本生成", icon="📖")
-    st.page_link("pages/page_02_media_understanding.py", label="视频理解", icon="🎞️")
-    st.page_link("pages/page_03_translation.py", label="文本翻译", icon="🇺🇳")
-    st.page_link("pages/page_04_travel_advisor.py", label="旅游顾问", icon="✈️")
-    st.page_link("pages/page_05_rag_search.py", label="RAG搜索", icon="🔍")
-    st.page_link("pages/page_06_media_search.py", label="媒体搜索", icon="🎥")
-    st.page_link("pages/page_07_image_generation.py", label="图片生成", icon="🎨")
-    st.page_link("pages/page_08_chatbot.py", label="聊天机器人", icon="💬")
-    st.page_link("pages/page_09_gaming_servicebot.py", label="游戏客服平台", icon="🤖")
-    st.page_link("pages/page_10_ecommerce_servicebot.py", label="电商客服平台", icon="🤖")
-    st.page_link("pages/page_11_claude_chatbot.py", label="Claude3.5聊天机器人", icon="💬")
-    st.page_link("pages/page_12_llama_chatbot.py", label="Llama3.1聊天机器人", icon="💬")
-    st.page_link("https://pantheon.corp.google.com/translation/hub", label="GCP翻译门户", icon="🌎")
-    st.page_link("https://pantheon.corp.google.com/vertex-ai/generative/multimodal/gallery", label="GCP控制台 - Gemini", icon="🌎")
-    st.page_link("https://pantheon.corp.google.com/gen-app-builder/engines", label="GCP控制台 - App Builder", icon="🌎")
+    st.page_link("homepage.py", label="Home", icon="🏠")
+    st.page_link("pages/page_01_text_generation.py", label="Text Generation", icon="📖")
+    st.page_link("pages/page_02_media_understanding.py", label="Media Understanding", icon="🎞️")
+    st.page_link("pages/page_03_translation.py", label="Text Translation", icon="🇺🇳")
+    st.page_link("pages/page_04_travel_advisor.py", label="Travel Advisor", icon="✈️")
+    st.page_link("pages/page_05_rag_search.py", label="RAG Search", icon="🔍")
+    st.page_link("pages/page_06_media_search.py", label="Media Search", icon="🎥")
+    st.page_link("pages/page_07_image_generation.py", label="Image Generation", icon="🎨")
+    st.page_link("pages/page_08_chatbot.py", label="Chatbot", icon="💬")
+    st.page_link("pages/page_09_gaming_servicebot.py", label="Gaming Servicebot", icon="🤖")
+    st.page_link("pages/page_10_ecommerce_servicebot.py", label="E-commerce Servicebot", icon="🤖")
+    st.page_link("pages/page_11_claude_chatbot.py", label="Claude 3.5 Chatbot", icon="💬")
+    st.page_link("pages/page_12_llama_chatbot.py", label="Llama 3.1 Chatbot", icon="💬")
+    st.page_link("https://pantheon.corp.google.com/translation/hub", label="GCP Translation Hub", icon="🌎")
+    st.page_link("https://pantheon.corp.google.com/vertex-ai/generative/multimodal/gallery", label="GCP Console - Gemini", icon="🌎")
+    st.page_link("https://pantheon.corp.google.com/gen-app-builder/engines", label="GCP Console - App Builder", icon="🌎")
     st.text("")
     st.subheader('', divider='rainbow')
     st.text("")
     st.markdown(
         """
-    ## 关于
-    这是由:blue[Google Cloud Vertex AI]驱动的生成式AI平台以及企业级RAG搜索引擎
+    ## About
+    This is a generative AI platform powered by :blue[Google Cloud Vertex AI] and an enterprise-ready RAG search engine
         """
     )
     st.page_link("https://cloud.google.com/vertex-ai?hl=en", label="Google Cloud Vertex AI", icon="☁️")
@@ -51,16 +51,16 @@ with st.sidebar:
     with cent_co:
         st.write(':grey[Powered by] **Vertex AI**')
 
-    st.page_link("pages/terms_of_service.py", label="用户服务协议", icon="📄")
-    st.page_link("pages/privacy_policy.py", label="用户隐私政策", icon="🔒")
+    st.page_link("pages/terms_of_service.py", label="Terms of Service", icon="📄")
+    st.page_link("pages/privacy_policy.py", label="Privacy Policy", icon="🔒")
 
 left_co, cent_co,last_co = st.columns([0.23,0.7,0.07])
 with cent_co:
-    st.title("GCP GenAI隐私政策")
+    st.title("GCP GenAI Privacy Policy")
 
 left_co, cent_co,last_co = st.columns([0.37,0.5,0.13])
 with cent_co:
-    st.write("生效日期: 2024年8月1日")
+    st.write("Effective Date: August 1, 2024")
 
 st.text("")
 
@@ -69,58 +69,58 @@ st.markdown("""
 
 
 
-本隐私政策描述了GCP GenAI应用（以下简称"本应用"）如何收集、使用和保护您的个人信息。我们重视您的隐私，并致力于保护您的个人数据。请仔细阅读本政策，了解我们的做法。
+This Privacy Policy describes how the GCP GenAI application (hereinafter referred to as "the Application") collects, uses, and protects your personal information. We value your privacy and are committed to protecting your personal data. Please read this policy carefully to understand our practices.
 
-## 1. 信息收集
+## 1. Information Collection
 
-1.1 本应用不收集、存储或处理任何用户个人信息。
+1.1 The Application does not collect, store, or process any user personal information.
 
-1.2 在使用本应用时，您需要使用您的Google公司账号登录。本应用仅验证您的邮件地址域名，以确认您是否有权限使用本应用，但不会记录或存储您的任何个人信息，包括但不限于姓名、邮件地址、电话号码等。
+1.2 When using the Application, you need to log in with your Google company account. The Application only verifies your email address domain to confirm whether you have permission to use the Application, but does not record or store any of your personal information, including but not limited to name, email address, phone number, etc.
 
-1.3 本应用没有任何形式的后端存储服务，包括但不限于数据库、磁盘以及对象存储服务，因此不会，也没有任何能力存储任何形式的个人信息或其他信息。
+1.3 The Application does not have any form of backend storage service, including but not limited to databases, disks, and object storage services, therefore it cannot and has no ability to store any form of personal information or other information.
 
-## 2. 信息使用
+## 2. Information Use
 
-2.1 本应用仅将用户提供的数据(如提示词、图片、视频或文档)即时传递给Google Cloud Vertex AI的相关API，并将API返回的结果即时显示给用户。
+2.1 The Application only immediately transmits the data provided by users (such as prompts, images, videos, or documents) to the relevant APIs of Google Cloud Vertex AI, and immediately displays the results returned by the API to users.
 
-2.2 由于本应用不会存储用户数据，因此不会，也没有能力将用户数据用于任何其他目的，更加不会与任何第三方共享用户数据。
+2.2 Since the Application does not store user data, it cannot and has no ability to use user data for any other purpose, let alone share user data with any third party.
 
-## 3. 数据安全
+## 3. Data Security
 
-3.1 由于本应用不存储任何用户数据，因此不存在数据泄露的风险。
+3.1 Since the Application does not store any user data, there is no risk of data leakage.
 
-3.2 所有数据传输都通过安全的HTTPS协议进行，以确保数据在传输过程中的安全性。
+3.2 All data transmission is done through secure HTTPS protocol to ensure data security during transmission.
 
-## 4. Cookie和跟踪技术
+## 4. Cookies and Tracking Technologies
 
-本应用不使用cookies或任何其他跟踪技术。每次登录都是一次全新的访问。
+The Application does not use cookies or any other tracking technologies. Each login is a brand new visit.
 
-## 5. 第三方服务
+## 5. Third-Party Services
 
-本应用使用Google Cloud Vertex AI的API。根据Google Cloud的使用政策，Google Cloud有权拒绝响应违反其"负责任的AI"政策的请求。
+The Application uses Google Cloud Vertex AI's API. According to Google Cloud's usage policy, Google Cloud has the right to refuse to respond to requests that violate its "Responsible AI" policy.
 
-## 6. 数据保留
+## 6. Data Retention
 
-6.1 本应用不保留任何用户数据。
+6.1 The Application does not retain any user data.
 
-6.2 当用户结束使用本应用(例如关闭浏览器或清空对话记录)后，所有在此过程中产生的数据和信息都将被即时清除。这是由本应用的代码机制和逻辑所决定的。
+6.2 When users finish using the Application (for example, closing the browser or clearing the conversation history), all data and information generated during this process will be immediately cleared. This is determined by the code mechanism and logic of the Application.
 
-## 7. 用户权利
+## 7. User Rights
 
-由于本应用不收集或存储任何个人数据，因此不涉及通常的数据主体权利，如访问权、更正权、删除权等。
+Since the Application does not collect or store any personal data, it does not involve the usual data subject rights such as the right of access, right of rectification, right of erasure, etc.
 
-## 8. 儿童隐私
+## 8. Children's Privacy
 
-本应用不面向18岁以下的个人提供服务，也不会故意收集18岁以下个人的任何信息。
+The Application does not provide services to individuals under 18 years of age and does not knowingly collect any information from individuals under 18 years of age.
 
-## 9. 国际数据传输
+## 9. International Data Transfer
 
-本应用可能涉及将数据传输到美国或其他国家/地区的Google服务器。但请注意，这种传输是即时的，不涉及数据存储。
+The Application may involve transferring data to Google servers in the United States or other countries/regions. However, please note that this transfer is instantaneous and does not involve data storage.
 
-## 10. 隐私政策的变更
+## 10. Changes to Privacy Policy
 
-本应用可能会不时更新本隐私政策。任何重大变更都将在本页面上公布。我们建议您定期查看本政策以了解任何更新。
+The Application may update this Privacy Policy from time to time. Any significant changes will be posted on this page. We recommend that you regularly review this policy to stay informed of any updates.
 
-## 11. 联系我们
+## 11. Contact Us
 
-如果您对本隐私政策有任何疑问或担忧，请联系本应用的开发者：""" + f"{st.secrets['developer_email']}" + "。")
+If you have any questions or concerns about this Privacy Policy, please contact the developer of the Application: """ + f"{st.secrets['developer_email']}" + ".")

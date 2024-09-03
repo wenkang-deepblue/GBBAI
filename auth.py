827a594a52fe14ea21cef3080a20a8030a7162df
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-09-03 14:37:25 +0800

    Update auth.py

--- auth.py
+++ auth.py
@@ -50,7 +50,7 @@ def login():
         }}
         .english-version-link {{
             position: fixed;
-            top: 120px;
+            top: 130px;
             right: 10px;
             z-index: 1000;
         }}

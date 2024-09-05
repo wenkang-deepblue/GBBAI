291956c6e74a645b0286194162f623f1b81d8fae
wenkang-deepblue <167299405+wenkang-deepblue@users.noreply.github.com>
2024-09-05 11:39:29 +0800

    Update page_08_chatbot.py

--- pages/page_08_chatbot.py
+++ pages/page_08_chatbot.py
@@ -77,7 +77,7 @@ with cent_co:
 left_co, cent_co,last_co = st.columns([0.43,0.38,0.29])
 with cent_co:
     st.caption(":blue[_Enterprise-ready Chatbot_]")
-st.image('https://storage.googleapis.com/ghackathon/page_18_zh.png')
+st.image('https://storage.googleapis.com/ghackathon/page_18_en.png')
 left_co, cent_co,last_co = st.columns([0.24,0.51,0.25])
 with cent_co:
     st.subheader('', divider='rainbow')
